e8a69d98aa456883187428d086e2847b58f6e0f1
anthonyevans29 <36042242+anthonyevans29@users.noreply.github.com>
2024-07-03 12:39:53 -0400

    Update alert-channels.ts

--- __checks__/alert-channels.ts
+++ __checks__/alert-channels.ts
@@ -1,5 +1,15 @@
-import { EmailAlertChannel, defineAlertChannel } from '@checkly/cli';
+/**
+* This is a Checkly CLI AlertChannel construct. To learn more, visit:
+* - https://www.checklyhq.com/docs/cli/
+* - https://www.checklyhq.com/docs/cli/constructs-reference/#alertchannel
+*/
 
-defineAlertChannel(new EmailAlertChannel('ACME Alerts', {
-  addresses: ['teamlead@acme.com'],
-}));
+import { EmailAlertChannel } from 'checkly/constructs'
+
+export const emailChannel = new EmailAlertChannel('email-221153', {
+  address: 'anthony.snc@gmail.com',
+  sendRecovery: true,
+  sendFailure: true,
+  sendDegraded: true,
+  sslExpiry: true,
+})
